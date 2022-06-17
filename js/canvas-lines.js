c179c6089c61a111c637c420c0555823a1ca8272
Jetso Analin <jetyjetso@gmail.com>
2022-06-17 22:15:09 +0530

    Coming Soon Website Initiated

---
+++ js/canvas-lines.js
@@ -0,0 +1,102 @@
+var mouseX = 0, mouseY = 0,
+windowHalfX = window.innerWidth / 2,
+windowHalfY = window.innerHeight / 2,
+SEPARATION = 200,
+AMOUNTX = 10,
+AMOUNTY = 10,
+camera, scene, renderer,
+bgContainer = document.querySelector('.custom-header-bg'),
+firstTime = true;
+document.addEventListener("DOMContentLoaded", function(event) {
+  if (typeof THREE === "undefined") {
+    return;
+  }
+
+  init();
+  animate();
+});
+function init() {
+  var container, separation = 100, amountX = 50, amountY = 50,
+  particles, particle;
+  container = document.createElement('div');
+  bgContainer.appendChild(container);
+  camera = new THREE.PerspectiveCamera( 75, bgContainer.clientWidth / bgContainer.clientHeight, 1, 10000 );
+  camera.position.z = 100;
+  scene = new THREE.Scene();
+  renderer = new THREE.CanvasRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( bgContainer.clientWidth, bgContainer.clientHeight + 50 );
+  container.appendChild( renderer.domElement );
+  // particles
+  var PI2 = Math.PI * 2;
+  var material = new THREE.SpriteCanvasMaterial( {
+    color: 0xffffff,
+    program: function ( context ) {
+      context.beginPath();
+      context.arc( 0, 0, 0.5, 0, PI2, true );
+      context.fill();
+    }
+  } );
+  var geometry = new THREE.Geometry();
+  for ( var i = 0; i < 100; i ++ ) {
+    particle = new THREE.Sprite( material );
+    particle.position.x = Math.random() * 2 - 1;
+    particle.position.y = Math.random() * 2 - 1;
+    particle.position.z = Math.random() * 2 - 1;
+    particle.position.normalize();
+    particle.position.multiplyScalar( Math.random() * 10 + 450 );
+    particle.scale.x = particle.scale.y = 10;
+    scene.add( particle );
+    geometry.vertices.push( particle.position );
+  }
+  // lines
+  var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5 } ) );
+  scene.add( line );
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+  document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+  //
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+  camera.aspect = bgContainer.clientWidth / bgContainer.clientHeight;
+  camera.updateProjectionMatrix();
+  if (firstTime) {
+    renderer.setSize( bgContainer.clientWidth, bgContainer.clientHeight + 50);
+    firstTime = false;
+  } else {
+    renderer.setSize( bgContainer.clientWidth, bgContainer.clientHeight );
+  }
+}
+//
+function onDocumentMouseMove(event) {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+}
+function onDocumentTouchStart( event ) {
+  if ( event.touches.length > 1 ) {
+    event.preventDefault();
+    mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    mouseY = event.touches[ 0 ].pageY - windowHalfY;
+  }
+}
+function onDocumentTouchMove( event ) {
+  if ( event.touches.length == 1 ) {
+    event.preventDefault();
+    mouseX = event.touches[ 0 ].pageX - windowHalfX;
+    mouseY = event.touches[ 0 ].pageY - windowHalfY;
+  }
+}
+//
+function animate() {
+  requestAnimationFrame( animate );
+  render();
+}
+function render() {
+  camera.position.x += ( mouseX - camera.position.x ) * .01;
+  camera.position.y += ( - mouseY + 200 - camera.position.y ) * .01;
+  camera.lookAt( scene.position );
+  renderer.render( scene, camera );
+}
